1c4e583ef13d617ba2fc33513188c5f981ffe865
Tony Phipps <tonyphipps@gmail.com>
2016-01-31 20:41:56 +0000

    tweek App.jsx to connect the note component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,5 +1,6 @@
 import uuid from 'node-uuid';
 import React from 'react';
+import Note from './Note.jsx';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -29,7 +30,9 @@ export default class App extends React.Component {
       <div>
         <button onClick={this.addNote}>+</button>
         <ul>{notes.map(note =>
-          <li key={note.id}>{note.task}</li>
+          <li key={note.id}>
+            <Note task={note.task} />
+          </li>
         )}</ul>
       </div>
     );
