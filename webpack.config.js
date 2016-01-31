ca40a2b658b79790ee8bca80b0732f751f562ae1
Tony Phipps <tonyphipps@gmail.com>
2016-01-31 18:20:58 +0000

    added babel loader declaration to webpack.congif.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,19 @@ const PATHS = {
 const common = {
   // Entry accepts a path or an object of entries
   entry: PATHS.app,
+
+  // Add resolve.extensions
+  // '' is needed to allow imports without an extension.
+  // Note the .'s before extensions as it will fail to mat without!!!
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
   },
+
   module: {
     loaders: [
       {
@@ -23,6 +32,16 @@ const common = {
         loaders: ['style', 'css'],
         // Include accepts either a path or an array of paths.
         include: PATHS.app
+      },
+
+      // Set up jsx. This accepts js too thanks to RegExp
+      {
+        test: /\.jsx?$/,
+        // Enable caching for improved performance during development
+        // It uses default OS directory by default. If you need something
+        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app,
       }
     ]
   }
