82e2e4cedc3549ebb5f1cc3eaf28c61e3cd06b7d
Tony Phipps <tonyphipps@gmail.com>
2016-01-31 20:10:51 +0000

    added button to the /app/component/App.jsx file which triggers  to add a new note to the list

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -27,10 +27,36 @@ export default class App extends React.Component {
 
     return (
       <div>
+        <button onClick={this.addNote}>+</button>
         <ul>{notes.map(note =>
           <li key={note.id}>{note.task}</li>
         )}</ul>
       </div>
     );
+  }
+
+  // We are using an experimental feature known as property
+  // initializer here. It allows us to bing the metho `this`
+  // to point at our *App* instance.
+  //
+  // Alternatively we could `bind` at `constructor` using
+  // a line, such as this.addNote = this.addNote.bing(this);
+  addNote = () => {
+    // It would be possible to write this in an imperative style.
+    // I.e., through `this.state.notes.push` and then
+    // `this.setState({notes: this.state.notes})` to commit.
+    //
+    // I tend to favor functional style whenever that makes sense.
+    // Even though it might take more code sometimes, I feel
+    // the benefits (easy to reason about, no side effects)
+    // more than make up for it.
+    //
+    // Libraries, such as Immutable.js, go a notch further.
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New Task'
+      }])
+    })
   }
 }
