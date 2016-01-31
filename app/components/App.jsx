2d3eb126f907b9f76702f35a0d0ff2d14d84cab5
Tony Phipps <tonyphipps@gmail.com>
2016-01-31 20:01:54 +0000

    converted notes const data structure into a component state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,21 +2,28 @@ import uuid from 'node-uuid';
 import React from 'react';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
+
   render() {
-    const notes = [
-      {
-        id: uuid.v4(),
-        task: 'Learn Webpack'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Learn React'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Do laundry'
-      }
-    ];
+    const notes = this.state.notes;
 
     return (
       <div>
