7845e00cda760941eeee98efbee8e99cfa502804
Tony Phipps <tonyphipps@gmail.com>
2016-01-31 20:52:43 +0000

    revert changes, used wrong file...

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,12 +1,3 @@
 import React from 'react';
-import Note from './Note.jsx';
 
-export default ({notes}) => {
-  return (
-    <ul>{notes.map(note =>
-      <li key={note.id}>
-        <Note task={note.task} />
-      </li>
-    )}</ul>
-  );
-}
+export default ({task}) => <div>{task}</div>;
